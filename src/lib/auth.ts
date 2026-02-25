d7e40706378cac77a2fed800ac8ee62d6a965983
alikernel <ali.barcelona360@gmail.com>
2026-02-25 11:41:29 -0600

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { LibsqlDialect } from "@libsql/kysely-libsql";
 export const auth = betterAuth({
     database: {
         dialect: new LibsqlDialect({
-            // استخدمت الاسم الموجود في صورتك بالضبط
+            // تأكدي أن هذا الاسم مطابق لصورتك بالضبط
             url: process.env.TURSO_DATABASE_URL || "", 
             authToken: process.env.TURSO_AUTH_TOKEN || "",
         }),
