d1de926c5af384402010c05ad9f32e5363f7daed
alikernel <ali.barcelona360@gmail.com>
2026-02-26 12:06:46 -0600

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from "better-auth";
-import { LibsqlDialect } from "@libsql/kysely-libsql";
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+    url: import.meta.env.TURSO_DATABASE_URL || "",
+    authToken: import.meta.env.TURSO_AUTH_TOKEN || "",
+});
 
 export const auth = betterAuth({
     database: {
-        dialect: new LibsqlDialect({
-            url: import.meta.env.TURSO_DATABASE_URL || "",
-            authToken: import.meta.env.TURSO_AUTH_TOKEN || "",
-        }),
+        db: client,
         type: "sqlite"
     },
     baseURL: "https://www.alikernel.com",
