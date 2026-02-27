3d7273f817d7f2403a9c28a3b9ed49314e07c14a
alikernel <ali.barcelona360@gmail.com>
2026-02-27 12:05:30 -0600

    Update [...auth].ts

--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -2,5 +2,7 @@ import { auth } from "../../../lib/auth";
 import type { APIRoute } from "astro";
 
 export const ALL: APIRoute = async (ctx) => {
-	return await auth.handler(ctx.request);
+    return await auth.handler(ctx.request);
 };
+
+export const prerender = false;
