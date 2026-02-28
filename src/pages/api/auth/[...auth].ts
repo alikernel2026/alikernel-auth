6cc300d941e13c3aa9f74c9b511df1abaf8649d7
alikernel <ali.barcelona360@gmail.com>
2026-02-27 23:50:28 -0600

    Update [...auth].ts

--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -1,9 +1,10 @@
-import { auth } from "../../../lib/auth";
+import { createAuth } from "../../../lib/auth";
 import type { APIRoute } from "astro";
 
 export const ALL: APIRoute = async (ctx) => {
-    const db = (ctx.locals as any).runtime.env.DB;
-    return await auth(db).handler(ctx.request);
+    const env = (ctx.locals as any).runtime.env;
+    const auth = createAuth(env);
+    return await auth.handler(ctx.request);
 };
 
 export const prerender = false;
