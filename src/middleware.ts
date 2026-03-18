8475ce85c8f6afe61850b2569e0cfbce6ec9a5c5
alikernel <ali.barcelona360@gmail.com>
2026-03-18 08:14:57 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,11 +11,11 @@ if (url.pathname.startsWith('/login') ||
     url.pathname.startsWith('/about') ||
     url.pathname.startsWith('/privacy') ||
     url.pathname.startsWith('/terms') ||
+    url.pathname.startsWith('/contact') ||
     url.pathname.startsWith('/content-transfer') ||
     url.pathname.startsWith('/content-center')) {
   return next();
   }
-
   const bloggerUrl = `https://alikernel.blogspot.com${url.pathname}${url.search}`;
   const response = await fetch(bloggerUrl);
   
